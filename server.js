9125b4e57fb22d55ffaa34504505d57104993e13
LeoCR <leonardoas2007@gmail.com>
2020-12-13 18:08:40 -0600

    Downloading xlsx files

--- server.js
+++ server.js
@@ -22,17 +22,17 @@ app.use(bodyParser.urlencoded({
     extended: false
 })); 
 
-app.use(bodyParser.json());
+app.use(bodyParser.json()); 
 
 app.post('/api/upload-xlsx',upload.single('image'),async(req,res)=>{
-    let tableData=[]; 
+    let tableData=[];  
     try {
-        var workbook = new Excel.Workbook();
+        const workbook = new Excel.Workbook();
         const originalFileName=req.file.originalname.toString();
         if(originalFileName.includes('.xlsx')){
-            await workbook.xlsx.readFile(req.file.path).then(function() {
-                workbook.eachSheet(function(worksheet, sheetId) { 
-                    let header=[]
+            let header=[]
+            await workbook.xlsx.readFile(req.file.path).then(function() { 
+                workbook.eachSheet(function(worksheet, sheetId) {  
                      worksheet.eachRow({ includeEmpty: false }, function(row, rowNumber) { 
                             if(rowNumber===1){
                                 row.eachCell(function(cell, colNumber) {
@@ -40,10 +40,7 @@ app.post('/api/upload-xlsx',upload.single('image'),async(req,res)=>{
                                     console.log('Cell ' + colNumber + ' = ' + cell.value);
                                 });
                             } 
-                    }); 
-                    tableData.push({
-                        header
-                    }) 
+                    });  
                     worksheet.eachRow({ includeEmpty: false }, function(row, rowNumber) { 
                         let cellValues=[] 
                         if(rowNumber>1){
@@ -51,13 +48,13 @@ app.post('/api/upload-xlsx',upload.single('image'),async(req,res)=>{
                                 cellValues.push(cell.value)
                                 console.log('Cell ' + colNumber + ' = ' + cell.value);
                             });
-                            tableData.push({
-                                tableContent:cellValues
-                            })  
+                            tableData.push(cellValues)  
                         }
                     }); 
                 }); 
-                res.json(tableData)
+                res.json({
+                    header:header,
+                    tableDataContent:tableData})
             })
             .catch((error)=>{
                 res.json({
@@ -80,6 +77,89 @@ app.post('/api/upload-xlsx',upload.single('image'),async(req,res)=>{
         })
     }
 })
+app.get('/api/download-xlsx',(req,res)=>{
+    try {
+        const jsonData={
+            header:[
+                'ProductName',
+                'ProductDescription',
+                'ProductPrice'
+            ],
+            tableDataContent:[
+                [
+                    "Product Name 1",
+                    "Product Description 1",
+                    94
+                ],
+                [
+                    "Product Name 2",
+                    "Product Description 2",
+                    97
+                ],
+                [
+                    "Product Name 3",
+                    "Product Description 3",
+                    98
+                ],
+                [
+                    "Product Name 4",
+                    "Product Description 4",
+                    32
+                ],
+                [
+                    "Product Name 5",
+                    "Product Description 5",
+                    45
+                ],
+                [
+                    "Product Name 6",
+                    "Product Description 6",
+                    97
+                ],
+                [
+                    "Product Name 7",
+                    "Product Description 7",
+                    94
+                ],
+                [
+                    "Product Name 8",
+                    "Product Description 8",
+                    94
+                ],
+            ]
+        }
+        const workbook = new Excel.Workbook();
+        const worksheet = workbook.addWorksheet("Maintenence");
+        worksheet.columns = [
+            {header: 'Product Name', key: 'productName', width: 20},
+            {header: 'Product Description', key: 'productDescription', width: 20},
+            {header: 'Product Price', key: 'productPrice', width:20}
+        ]
+        worksheet.getRow(1).font = {bold: true}
+        worksheet.getCell('A1').alignment = { vertical: 'center', horizontal: 'center' };
+        worksheet.getCell('B1').alignment = { vertical: 'center', horizontal: 'center' }
+        worksheet.getCell('C1').alignment = { vertical: 'center', horizontal: 'center' }
+        for (let tableDataIndex = 0; tableDataIndex < jsonData.tableDataContent.length; tableDataIndex++) {
+            const tableContentElement = jsonData.tableDataContent[tableDataIndex];
+            worksheet.addRow({productName: tableContentElement[0], productDescription: tableContentElement[1],productPrice:tableContentElement[2]}).alignment = { vertical: 'center', horizontal: 'center' };
+        } 
+        res.setHeader(
+            "Content-Type",
+            "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",    
+            "Content-Disposition",
+            "attachment; filename=" + "tableDataContent.xlsx"
+        );
+    
+        return workbook.xlsx.write(res).then(function () {
+            res.status(200).end();
+        });
+    } catch (error) {
+        res.json({
+            error:true,
+            errorMessage:error
+        })
+    }
+}) 
 app.listen(45398, () => {
     console.log(`http://localhost:45398`)
 }) 
